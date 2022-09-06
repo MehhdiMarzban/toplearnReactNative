2478c32e2252c8d55742be1ebecfeec08eca3988
mehdi marzban <marzban7272@gmail.com>
2022-09-06 23:09:17 +0430

    fix some bugs

--- app/screens/index.js
+++ app/screens/index.js
@@ -2,6 +2,6 @@ export { default as WelcomeScreen } from "./WelcomeScreen";
 export { default as LoginScreen } from "./LoginScreen";
 export { default as RegisterScreen } from "./RegisterScreen";
 export { default as ProfileScreen } from "./HomeScreens/ProfileScreen";
-export { default as CoursesScreen } from "./HomeScreens/CoursesScreen";
-export { default as MyCoursesScreen } from "./HomeScreens/MyCoursesScreen";
+export { default as CoursesScreen } from "./HomeScreens/CouresScreen/CoursesScreen";
+export { default as MyCoursesScreen } from "./HomeScreens/CouresScreen/MyCoursesScreen";
 export { default as SingleCourseScreen } from "./HomeScreens/CouresScreen/SingleCourseScreen";
